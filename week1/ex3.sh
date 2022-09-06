48852c95e7553edc60044ee2a04cd8a3192b2103
skylemn07 <88384986+skylemn07@users.noreply.github.com>
2022-09-06 10:08:47 +0300

    hw

---
+++ week1/ex3.sh
@@ -0,0 +1,17 @@
+cd ~/week1
+date
+sleep 3
+mkdir for_home
+cd for_home
+ls -ltr ~ > home.txt
+cat home.txt
+ls
+cd ~/week1
+date
+sleep 3
+mkdir for_root
+cd for_root
+ls -ltr / > root.txt
+cat root.txt
+ls
+
