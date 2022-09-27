44d3e1cda79673a6cae78e966aa345b7a52519fe
skylemn07 <AnastasiaPalashkina@gmail.com>
2022-09-27 21:43:59 +0300

    New commit

--- week04/ex4.c
+++ week04/ex4.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 1001
 
@@ -9,7 +10,12 @@ int main() {
     while(1) {
         printf("> ");
         fgets(command, 1001, stdin);
-        system(command);
+        int pid = fork();
+        if (pid == 0){
+            system(command);
+            exit(EXIT_SUCCESS);
+        }
+        
     }
 
     return EXIT_SUCCESS;
